059ba17c0e7d0f421d0d2126558d065c1cdac722
Joshua Yancy <joshua.yancy@gmail.com>
2013-02-05 19:37:49 -0500

    finished

--- src/TCPEchoClient.java
+++ src/TCPEchoClient.java
@@ -1,3 +1,7 @@
+// Joshua Yancy HW 1 CS 471G
+//Client side sending message count
+
+
 import java.net.Socket;
 import java.net.SocketException;
 import java.io.IOException;
@@ -8,14 +12,15 @@ public class TCPEchoClient {
 
   public static void main(String[] args) throws IOException {
 
-    if ((args.length < 2) || (args.length > 3))  // Test for correct # of args
-      throw new IllegalArgumentException("Parameter(s): <Server> <Word> [<Port>]");
+    if ((args.length < 3) || (args.length > 4))  // Test for correct # of args
+      throw new IllegalArgumentException("Parameter(s): <Server> [<Port>] <Count> <Word> ");
 
     String server = args[0];       // Server name or IP address
     // Convert argument String to bytes using the default character encoding
-    byte[] data = args[1].getBytes();
-
-    int servPort = (args.length == 3) ? Integer.parseInt(args[2]) : 7;
+    String message = args[2] + " " + args[3];
+    byte[] data = message.getBytes();
+    
+    int servPort = (args.length == 4) ? Integer.parseInt(args[1]) : 7;
 
     // Create socket that is connected to server on specified port
     Socket socket = new Socket(server, servPort);
@@ -25,18 +30,18 @@ public class TCPEchoClient {
     OutputStream out = socket.getOutputStream();
 
     out.write(data);  // Send the encoded string to the server
-
+    int bytelength = message.length() * 3; 
     // Receive the same string back from the server
     int totalBytesRcvd = 0;  // Total bytes received so far
     int bytesRcvd;           // Bytes received in last read
-    while (totalBytesRcvd < data.length) {
-      if ((bytesRcvd = in.read(data, totalBytesRcvd,  
-                        data.length - totalBytesRcvd)) == -1)
+    byte[] newdata = new byte[bytelength];
+    while (totalBytesRcvd < newdata.length) {
+      if ((bytesRcvd = in.read(newdata, totalBytesRcvd,  newdata.length - totalBytesRcvd)) == -1)
         throw new SocketException("Connection closed prematurely");
       totalBytesRcvd += bytesRcvd;
     }  // data array is full
-
-    System.out.println("Received: " + new String(data));
+   
+    System.out.println("Received: " + new String(newdata));
 
     socket.close();  // Close the socket and its streams
   }
--- src/TCPEchoServer.java
+++ src/TCPEchoServer.java
@@ -1,4 +1,11 @@
+// Joshua Yancy HW 1 CS 471G
+//Server side receiving message count
+// returns message count number of times
+
+
+
 import java.net.*;  // for Socket, ServerSocket, and InetAddress
+import java.nio.charset.CharsetEncoder;
 import java.io.*;   // for IOException and Input/OutputStream
 
 public class TCPEchoServer {
@@ -14,22 +21,42 @@ public class TCPEchoServer {
 
     // Create a server socket to accept client connection requests
     ServerSocket servSock = new ServerSocket(servPort);
-
+    System.out.println(InetAddress.getLocalHost().getHostAddress());
     int recvMsgSize;   // Size of received message
     byte[] receiveBuf = new byte[BUFSIZE];  // Receive buffer
-
+    
+   
     while (true) { // Run forever, accepting and servicing connections
       Socket clntSock = servSock.accept();     // Get client connection
 
       SocketAddress clientAddress = clntSock.getRemoteSocketAddress();
       System.out.println("Handling client at " + clientAddress);
-      
+      String[] splitdecoded;
       InputStream in = clntSock.getInputStream();
       OutputStream out = clntSock.getOutputStream();
-
+      // need to figure out how to get this to send back multiple times
+      // are we allowed to encode and decode
       // Receive until client closes connection, indicated by -1 return
       while ((recvMsgSize = in.read(receiveBuf)) != -1) {
-        out.write(receiveBuf, 0, recvMsgSize);
+    	  String decoded = new String(receiveBuf, "UTF-8");
+    	  String holder = "";
+    	  splitdecoded = decoded.split(" ");
+    	  int count = Integer.parseInt(splitdecoded[0]);
+    	  holder = splitdecoded[1];
+    	   for(int number = 1; number < count; number++)
+    	  {
+    		  holder = holder.trim() + " " + splitdecoded[1];
+    		  
+    	  }
+    	  byte[] convertme = holder.getBytes();
+    	  
+    	  byte[] result = new byte[convertme.length];
+    	  for(int i=0; i<convertme.length; i++)
+    	  {
+    		  result[i] = convertme[i];
+    	  }
+    	
+    	 out.write(result);
       }
 
       clntSock.close();  // Close the socket.  We are done with this client!
